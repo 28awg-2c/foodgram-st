1ebc79237c72f342038f73997a8f0ebfbd39c376
Anastasia Kuklina <kuklina0404@mail.ru>
2025-05-29 23:04:17 +0300

    fix foodgram-app/models.py

--- backend/foodgram/foodgram_app/models.py
+++ backend/foodgram/foodgram_app/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from user_login.models import User
 
+MIN_COOK_AND_AMOUNT = 1
+MAX_COOK_AND_AMOUNT = 32000
+MAX_NAME_LENGTH = 200
+MAX_MEASUREMENT_LENGTH = 50
+
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    measurement_unit = models.CharField(max_length=50)
+    name = models.CharField(max_length=MAX_NAME_LENGTH, unique=True,
+                            verbose_name='Название ингредиента')
+    measurement_unit = models.CharField(max_length=MAX_MEASUREMENT_LENGTH,
+                                        verbose_name='Мера')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
 
     def __str__(self):
         return f"{self.name} ({self.measurement_unit})"
@@ -14,17 +26,27 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='resipe')
-    name = models.CharField(max_length=200)
+        related_name='resipe',
+        verbose_name='Автор')
+    name = models.CharField(max_length=MAX_NAME_LENGTH,
+                            verbose_name='Название рецепта')
     image = models.ImageField(
-        upload_to='static/posts/')
-    text = models.TextField()
+        upload_to='static/posts/', verbose_name='Фото')
+    text = models.TextField(verbose_name='Рецепт')
     ingredients = models.ManyToManyField(
         Ingredient,
         through='RecipeIngredient',
-        related_name='recipes'
+        related_name='recipes',
+        verbose_name='Ингредиенты'
     )
-    cooking_time = models.PositiveIntegerField()
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления', min_length=MIN_COOK_AND_AMOUNT,
+        max_length=MAX_COOK_AND_AMOUNT)
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
 
     def __str__(self):
         return self.name
@@ -32,9 +54,16 @@ class Recipe(models.Model):
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name='recipe_ingredient')
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    amount = models.PositiveIntegerField()
+        Recipe, on_delete=models.CASCADE,
+        related_name='recipe_ingredient',
+        verbose_name='Рецепт')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,
+                                   verbose_name='Ингредиент')
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Кол-во ингредиента', min_length=MIN_COOK_AND_AMOUNT,
+        max_length=MAX_COOK_AND_AMOUNT)
 
     class Meta:
+        ordering = ['recipe']
+        verbose_name = 'Рецепт/Ингредиент'
         unique_together = ('recipe', 'ingredient')
