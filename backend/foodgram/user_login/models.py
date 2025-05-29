55bb176299b95337428a39f1abf7c0e0b8bdebb8
Anastasia Kuklina <kuklina0404@mail.ru>
2025-05-30 02:32:58 +0300

    fix models

--- backend/foodgram/user_login/models.py
+++ backend/foodgram/user_login/models.py
@@ -1,45 +1,52 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+MAX_LENGTH_TEXT = 150
+MAX_LENGTH_PASSWORD = 128
+MAX_LENGTH_EMAIL = 254
+
 
 class User(AbstractUser):
     username = models.CharField(
-        max_length=150,
+        max_length=MAX_LENGTH_TEXT,
         unique=True,
         error_messages={
             'unique': ("Пользователь с таким никнеймом уже существует."),
         },
+        verbose_name='Никнейм',
     )
     email = models.EmailField(
-        max_length=254,
+        max_length=MAX_LENGTH_EMAIL,
         unique=True,
         error_messages={
             'unique': ("Пользователь с таким email уже существует."),
         },
+        verbose_name='Почта',
     )
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='custom_user_set',
         blank=True,
-        verbose_name='groups',
+        verbose_name='Группы',
         help_text='The groups this user belongs to...',
     )
     user_permissions = models.ManyToManyField(
         'auth.Permission',
         related_name='custom_user_set',
         blank=True,
-        verbose_name='user permissions',
+        verbose_name='Права пользователей',
         help_text='Specific permissions for this user...',
     )
     avatar = models.ImageField(
         upload_to='static/users_avatars/',
         null=True,
-        blank=True
+        blank=True,
+        verbose_name='Фото'
     )
 
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=MAX_LENGTH_TEXT)
+    last_name = models.CharField(max_length=MAX_LENGTH_TEXT)
+    password = models.CharField(max_length=MAX_LENGTH_PASSWORD)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
@@ -80,6 +87,7 @@ class Follow(models.Model):
                 name='prevent_self_follow'
             )
         ]
+        ordering = ['-date_joined']
 
     def __str__(self):
         return f'{self.follower} подписан на {self.author}'
